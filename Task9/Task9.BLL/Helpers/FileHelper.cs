cc68f616a124782bf09ea2f5e6097c144db1ffd1
Oleg <User@DESKTOP-2L28T8V>
2018-05-09 18:49:47 +0300

    Creating App

---
+++ Task9/Task9.BLL/Helpers/FileHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace Task9.BLL.Helpers
+{
+	public class FileHelper
+	{
+		public static void SaveFile(string text)
+		{
+			var saveDlg = new SaveFileDialog
+			{
+				Title = "Browse for a save location",
+				FileName = "Data.txt",
+				Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+				InitialDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName,
+				OverwritePrompt = true
+			};
+
+			if (saveDlg.ShowDialog() == DialogResult.OK)
+			{
+				var sw = new StreamWriter(saveDlg.FileName);
+				sw.Write(text);
+				sw.Close();
+			}
+		}
+	}
+}
---
+++ Task9/Task9.UI/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Win32;
+using Task9.BLL.Helpers;
+
+namespace Task9.UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Save_File(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				FileHelper.SaveFile(TxbTextFile.Text);
+			}
+			catch (Exception exception)
+			{
+				TxbTextFile.Text = "ERROR: " + exception.Message;
+			}
+		}
+	}
+}
